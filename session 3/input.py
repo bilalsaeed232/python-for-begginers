5e3a0d426bee1a727868bd69a013e03184da8ebb
Bilal Saeed <bilalsaeed232@gmail.com>
2025-07-01 11:03:04 +0000

    session 3 classwork

---
+++ session 3/input.py
@@ -0,0 +1,30 @@
+# n = input("enter your name:")
+
+# print(type(n))
+
+# # if n == "umar":
+# #   print("Welcome", n)
+# # else:
+# #   print("Passa mara!")
+
+
+# marks = input("enter your marks:")
+
+# # e.g int : 1 , 2 , 3 , 4
+# marks = int(marks)
+
+# print("marks are:", marks)
+
+# #e.g float(marks):  1.0, 2.2, 4.5, 3.8
+
+# if marks > 90:
+#   print("grade A+")
+# else:
+#   print("grade is Not A+")
+
+
+n1 = input("enter first number:")
+
+n1 = float(n1)
+
+print(n1)
