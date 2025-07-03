c2eb372524455af9db5bd5bf7d43ca8a6aa5fb49
Bilal Saeed <bilalsaeed232@gmail.com>
2025-07-03 20:27:50 +0000

    session 4

---
+++ session 4/func.py
@@ -0,0 +1,14 @@
+def multiply(n1, n2):
+    x = n1 * n2
+    return x
+
+
+x = multiply(3, 4)
+
+print(x)
+
+
+x2 = x + 2
+print(x2)
+
+
---
+++ session 4/list.py
@@ -0,0 +1,64 @@
+# n = 4
+
+# print(n)
+
+# student1_name = "umar"
+# student2_name = "hassaan"
+# student3_name = "sudais"
+
+# print(student1_name)
+# print(student2_name)
+# print(student3_name)
+
+# List ======> collection of values
+
+student_names = ["umar", "hassaan", "sudais"]
+# print(student_names)
+
+# n = input("enter a numer")
+# n = int(n)
+
+
+# r = list(range(10))
+# print(r)
+
+
+# for i in student_names:
+#     print(i)
+
+even_numbers = [2,4,6,8,10,12,"Ali", 14]
+
+# print(even_numbers)
+
+
+# for i in even_numbers:
+#     if i == 8:
+#         print("EIGHT")
+#     else:
+#         print(i)
+
+
+# name = input("enter your name:")
+
+# n = list(name)
+
+# print(n)
+
+#e.g  n = ['u', 'm', 'a', 'r', ' ', 'q', 'a', 's', 'i', 'm']
+
+# # count number of 'a' characters in name
+# count = 0
+# for i in n:
+#     if i == "a":
+#         count = count + 1
+
+
+# print("Total number of 'a' in your name:", count)
+
+# to check lenght of list, i.e how many items in the list
+# print(len(student_names))
+
+
+
+fruits = ["apple", "orange", "banana", "kiwi", "watermelon"]
+print(len(fruits))
