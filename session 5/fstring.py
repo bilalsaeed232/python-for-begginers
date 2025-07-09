99b5a9799d363dfc2e9e3215e7342a194f3404b0
Bilal Saeed <bilalsaeed232@gmail.com>
2025-07-09 18:27:51 +0000

    session 5

---
+++ session 5/fstring.py
@@ -0,0 +1,30 @@
+# def table(n):
+#   if n == 2:
+#     print("passa mara 2 table am darla na darzi")
+#     return
+
+#   if n == 0:
+#     print("invalid number:",n)
+#     return
+
+#   print("***************")
+#   for i in range(10):
+#     i = i + 1
+#     x = i * n
+#     # print(n, "x",i, "=", x)
+#     print(f"{n} x {i} = {x}")
+
+
+
+# table(3)
+
+
+wiki = """we will rock programming together 🚀🚀🚀🚀🚀
+
+😉
+😉😉😉
+😉😉
+"""
+
+
+print(wiki)
---
+++ session 5/list2.py
@@ -0,0 +1,73 @@
+# def table(n):
+#   t = type(n)
+#   print(t)
+#   if t != int:
+#     print("invalid parameter")
+#     return 
+  
+#   if n == 2:
+#     print("passa mara 2 table am darla na darzi")
+#     return
+
+#   if n == 0:
+#     print("invalid number:",n)
+#     return
+
+#   print("***************")
+#   for i in range(10):
+#     i = i + 1
+#     x = i * n
+#     # print(n, "x",i, "=", x)
+#     print(f"{n} x {i} = {x}")
+# table(names)
+
+fruits = ["apples", "banana", "blueberies", "cherris", "cantalupes","dates", "figs","grapes","kiwis", "mangos"]
+
+# print(fruits[2])
+
+# print(fruits[-3])
+
+# print(fruits[1:4])
+
+# print(fruits[1:6:2])
+
+# print(fruits[:7])
+
+# print(fruits[3:])
+
+# integer, bool(True, False), string, float
+
+# before
+# print(fruits)
+
+# fruits[-4] = "Watermelon"
+
+# # after
+# print(fruits)
+
+
+# before
+# print(fruits)
+
+# fruits[1:4] = ["Orange", "strawberry"]
+
+
+# # after
+# print(fruits)
+
+# before
+print(fruits)
+
+
+fruits.append("Khataki")
+# after
+print(fruits)
+
+
+fruits.reverse()
+
+print(fruits)
+
+
+
+
