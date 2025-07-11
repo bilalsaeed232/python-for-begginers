54416a41911b36423da3f18805e9e87d30bec962
Bilal Saeed <bilalsaeed232@gmail.com>
2025-07-11 09:12:43 +0000

    session 7

---
+++ session 7/builtinfuncs.py
@@ -0,0 +1,10 @@
+name = "umar"
+
+print(name.upper())
+print(name.capitalize())
+
+
+statement = "the quick brown fox jumps over the lazy dog dog dog"
+
+print(statement.count("dog"))
+
---
+++ session 7/revision.py
@@ -0,0 +1,181 @@
+# n = 5
+
+# if n > 3:
+#     print("N IS GREATER THAN 3")
+
+
+# Programming concept: Functions
+
+# num = input("Enter a number:")
+# num = int(num)
+# for i in range(1, 11): # [1,2,3,4,5,6,7,8,9,10]
+#     n = i * num
+#     # print(x)
+#     print(num, "x", i, "=", n)
+
+
+# x1 = 23
+
+# if x1 > 12:
+#     print("x1 is greater")
+
+
+# print("hello world")
+
+# num = input("Enter a number:")
+# num = int(num)
+# for i in range(1, 11): # [1,2,3,4,5,6,7,8,9,10]
+#     n = i * num
+#     # print(x)
+#     print(num, "x", i, "=", n)
+
+
+# # dsfsdfdsfsd
+# # dsfsdfdsfsd
+# # dsfsdfdsfsd
+# # dsfsdfdsfsd
+
+
+# num = input("Enter a number:")
+# num = int(num)
+# for i in range(1, 11): # [1,2,3,4,5,6,7,8,9,10]
+#     n = i * num
+#     # print(x)
+#     print(num, "x", i, "=", n)
+
+# builtin functions: print(), input()
+# user defined functions: e.g table()
+
+# x1 = 123
+# syntax for function: def function_name():
+# user defined function below
+def table(num):
+    if num == 0:
+        return
+    
+    num = int(num)
+    for i in range(1, 11): # [1,2,3,4,5,6,7,8,9,10]
+        n = i * num
+        # print(x)
+        print(num, "x", i, "=", n)
+
+
+# n1 = input("Enter a number for table:")
+# table(n1)
+
+
+# table(9)
+
+# table(4)
+
+
+
+# num = input("Enter a number:")
+# num = int(num)
+# for i in range(1, 11): # [1,2,3,4,5,6,7,8,9,10]
+#     n = i * num
+#     # print(x)
+#     print(num, "x", i, "=", n)
+
+# y = 60
+
+# print("hello", 223, y)
+
+# e.g add(2, 6)
+# The sum of 2 and 6 is 8
+# def add(x, y):
+#     z = x + y
+#     print("The sum of",x, "and", y, "is",z)
+
+
+# add(2, 10)
+# add(45, 10)
+# add(5, 23)
+
+
+# Programing Concept: While loop
+# basically two types of loops: for-loop, while-loop
+
+# already know how many times i want the loop
+# for i in range(10):
+#     print(i)
+
+
+# doesn't already know how many time i want the loop to run
+# e.g
+# Enter a number for table (or 0 to 'exit'): 3
+# 3 x 1 = 3
+# .....
+# n = int(input("Enter a number for table (or 0 to 'exit'):"))
+
+# # not equal operator: !=
+# while n != 0:
+#     table(n)
+#     n = int(input("Enter a number for table (or 0 to 'exit'):"))
+
+# print("byeeee")
+
+########################################################################
+
+# Lists in Python
+
+# n1 = 6
+# n2 = 12
+# x = n1 + n2
+
+# print(x)
+
+# student names
+# student1_name = "umar"
+# student2_name = "hasaan"
+# student3_name = "amir"
+
+# print(student1_name)
+# print(student2_name)
+# print(student3_name)
+
+
+# better alternative
+
+student_names = [ "umar", "hasaan", "amir","muhammad", "ali", "waqas" ]
+
+# for name in student_names:
+#     print(name)
+
+# print(student_names)
+
+# for i in range(10): #[0,1,2,3,4,5,6,7,8,9]
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
